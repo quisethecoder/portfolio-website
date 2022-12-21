87078a857e61a3f25e78fbca78e6b0db6e009e09
quisethecoder <marquise_dawkins@aol.com>
2022-12-21 16:42:26 -0500

    inital commit/final version

---
+++ index.js
@@ -0,0 +1,97 @@
+gsap.from(".marq", {duration: 3, opacity: 0.3, y: -100});
+gsap.from(".fronte", {duration: 3, opacity: 0.3, y: -100});
+
+
+
+gsap.registerPlugin(ScrollTrigger)
+
+// Toggle Action keywords are play, pause, resume, reverse, restart, reset, complete, none
+
+
+gsap.from(".front", {
+    scrollTrigger:{
+        trigger: ".front",
+        toggleActions: "restart none none reset "
+    }, 
+    duration: 1.8,
+    opacity: 0.2, 
+    y: -100,
+    
+    // rotation: 360,
+    
+     
+    
+});
+
+
+gsap.to(".skills-word",{
+    scrollTrigger:{
+        trigger: ".skills-word",
+        toggleActions: "restart none none reset"
+    },
+    x: -20,
+    duration: 3
+});
+
+gsap.to(".logo",{
+    scrollTrigger:{
+        trigger: ".logo",
+        toggleActions: "restart none none reset"
+    },
+    x: -20,
+    duration: 3,
+    rotation: 360
+});
+
+gsap.to(".logo-b",{
+    scrollTrigger:{
+        trigger: ".logo-b",
+        toggleActions: "restart none none reset"
+    },
+    x: -20,
+    duration: 3,
+    rotation: -360
+});
+
+gsap.to(".logos",{
+    scrollTrigger:{
+        trigger: ".logos",
+        toggleActions: "restart none none reset"
+    },
+    x: -20,
+    duration: 3,
+    rotation: 360
+});
+
+
+gsap.to(".img-with-text",{
+    scrollTrigger:{
+        trigger: ".img-with-text",
+        toggleActions: "restart none none reset"
+    },
+    x: -20,
+    duration: 3
+});
+
+gsap.to(".img-sec-two",{
+    scrollTrigger:{
+        trigger: ".img-with-text",
+        toggleActions: "restart none none reset"
+    },
+    x: 10,
+    duration: 3
+});
+
+document.getElementById('dmbutton').addEventListener('click', function() {
+    const css = document.getElementById('cssfile');
+    const dmb = document.getElementById('dmbutton');
+
+    if (dmb.innerHTML == 'Dark Mode') {
+        css.setAttribute('href', './CSS/darkmode.css');
+        dmb.innerHTML = 'Light Mode'
+    }else{ css.setAttribute('href', 'CSS/styles.css');
+    dmb.innerHTML = 'Dark Mode'
+
+    }
+
+});
